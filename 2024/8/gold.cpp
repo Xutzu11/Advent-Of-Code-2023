bf5e8d6c5c715b70d1e6f300f86c2d8475ca2c7b
Ignat Alex-Matei <ignatalexmatei@yahoo.com>
2024-12-08 16:35:52 +0200

    AOC 2024: Problem 8

---
+++ 2024/8/gold.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+unordered_map<char,vector<pair<int,int>>> antennas; 
+vector<string> v;
+string s;
+int n, m, res = 0;
+
+bool valid(int i, int j) {
+    if (i < 0 || j < 0 || i >= n || j >= m)
+        return 0;
+    return 1;
+}
+
+int main() {
+    freopen("file.in", "r", stdin);
+    freopen("file.out", "w", stdout);
+    while (cin >> s) {
+        v.push_back(s);
+    }
+    n = v.size(); m = v[0].size();
+    for (int i=0;i<n;i++) {
+        for (int j=0;j<m;j++) {
+            if (v[i][j] != '.') {
+                antennas[v[i][j]].push_back({i, j});
+            }
+        }
+    }
+    for (auto &k: antennas) {
+        for (int i=0;i<k.second.size();i++) {
+            for (int j=i+1;j<k.second.size();j++) {
+                int ni, nj, off_i, off_j;
+                ni = k.second[i].first+(k.second[i].first-k.second[j].first);
+                off_i = (k.second[i].first-k.second[j].first);
+                nj = k.second[i].second+(k.second[i].second-k.second[j].second);
+                off_j = (k.second[i].second-k.second[j].second);
+                while (valid(ni, nj)) {
+                    v[ni][nj] = '#';
+                    ni += off_i;
+                    nj += off_j;
+                }
+                ni = k.second[j].first+(k.second[j].first-k.second[i].first);
+                off_i = (k.second[j].first-k.second[i].first);
+                nj = k.second[j].second+(k.second[j].second-k.second[i].second);
+                off_j = (k.second[j].second-k.second[i].second);
+                while (valid(ni, nj)) {
+                    v[ni][nj] = '#';
+                    ni += off_i;
+                    nj += off_j;
+                }
+            }
+        }
+    }
+    for (int i=0;i<n;i++) {
+        for (int j=0;j<m;j++) {
+            res += (v[i][j] != '.');
+        }
+    }
+    cout << res;
+    return 0;
+}
